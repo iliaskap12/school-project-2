7ad610c0abe7f3c2540a6d7b0173dd61a213426a
iliaskap12 <iliaskapareliotis@gmail.com>
2022-01-18 19:25:55 +0200

    Add handlebars library on front end at index.

--- controllers/handlebars.js
+++ controllers/handlebars.js
@@ -6,4 +6,19 @@ function searchResults(req, res, next) {
     res.sendFile(path.join(views, 'search.hbs'));
 }
 
+function header(req, res, next) {
+    res.sendFile(path.join(views, 'header.hbs'));
+}
+
+function footer(req, res, next) {
+    res.sendFile(path.join(views, 'footer.hbs'));
+}
+
+function thanks(req, res, next) {
+    res.sendFile(path.join(views, 'thanks.hbs'));
+}
+
+exports.thanks = thanks;
+exports.footer = footer;
+exports.header = header;
 exports.searchResults = searchResults;
